058f73475e4a7410e75fa2b113b25d2a4809eccf
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-03-16 20:42:31 +0100

    remove old files

--- src/PagedResult.ts
+++
@@ -1,4 +0,0 @@
-export class PagedResult<T>{
-    public data: T[];
-    public count: number;
-}
--- src/helloWorld.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ao-hello-world',
-  template: 'Hello world from the {{ projectTitle }} module!'
-})
-export class HelloWorldComponent {
-  public projectTitle: string = 'Angular OData Library (es5)';
-}
